2fe2a50d9dd3949187c9497aa0d07c579fbf6576
Ruan Rocha <ruansouza@alu.ufc.br>
2020-08-30 23:18:58 -0300

    Atividade 5

--- atividade_2/src/components/Questão 1/arena.jsx
+++ atividade_2/src/components/Questão 1/arena.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default (props) =>
     <div class="container mb-5">
         <h1>{props.arena}</h1>
-        <div class="row d-flex justify-content-center">
+        <div className="row d-flex justify-content-center">
             {React.Children.map(props.children, child => {
                 return React.cloneElement(child, {...props})
             })}
